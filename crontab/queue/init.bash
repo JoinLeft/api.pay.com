6cd5b65279c3eb4939b76244064af26851b1cd8d
姜梦林 <jiangmenglin@top-list.com.cn>
2018-04-16 21:19:34 +0800

    ok

--- crontab/queue/init.bash
+++ crontab/queue/init.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 启动程序
 function start(){
-    nohup /Applications/MAMP/bin/php/php7.1.8/bin/php /Applications/MAMP/htdocs/index.php >> /Applications/MAMP/htdocs/output 2>&1 &
+    nohup /usr/local/php/bin/php /data/wwwroot/api.test.com/crontab/queue/Resque.php >> /data/wwwlogs/output 2>&1 &
 }
 # 关闭程序
 function stop(){
